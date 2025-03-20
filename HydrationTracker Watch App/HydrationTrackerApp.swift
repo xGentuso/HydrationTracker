8677ec0acd14ccf514f4862fccd7dbe82220d05b
xGentuso <ryan.mota@triosstudent.com>
2025-03-20 08:34:55 -0400

    Initial Commit

---
+++ HydrationTracker Watch App/HydrationTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationTrackerApp.swift
+//  HydrationTracker Watch App
+//
+//  Created by ryan mota on 2025-03-20.
+//
+
+import SwiftUI
+
+@main
+struct HydrationTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
